7662d8746d77287149e00f2f9edd207376894b36
kunwoo park <scv74502@gmail.com>
2023-01-06 10:05:29 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,10 @@
+def solution(X, Y):
+    answer = ''
+    for i in range(9, -1, -1):
+        answer += str(i) * min(X.count(str(i)), Y.count(str(i)))
+    if answer == '':
+        return "-1"
+    elif answer.count("0") == len(answer):
+        return "0"
+    else:
+        return answer
