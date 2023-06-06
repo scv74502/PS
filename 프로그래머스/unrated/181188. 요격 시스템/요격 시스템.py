b0fba035cf87f8b0e783ac9b515cfeec934a0f83
kunwoo park <scv74502@gmail.com>
2023-06-06 16:54:19 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,33 +1,24 @@
 def solution(targets):
+    
+    # 시작점을 기준으로 오름차순 정렬하기
     targets.sort()
-    idx = 0
-    answer = 0
-    
-    # print(targets[0][1]-1)
-    
-    # 정렬된 첫 타겟을 꺼내서 요격 사거리 내에 넣음
-    idx = targets[0][1] - 1
-    
-    # 요격 사거리에 들어간 첫 타겟을 삭제하기
-    targets.pop(0)
-    
-    # 미사일 한 발을 사용함
-    answer += 1
-    
     # print(targets)
     
+    # 처음으로 정렬된 점을 뽑아서 설정하기
+    idx = targets[0][1] - 1
+    targets.pop(0)
+    # 초기에 미사일 하나 소비함
+    answer = 1
+    
     for target in targets:
-        
-        # print(idx)
-        # print(target)
-        
-        # 현재 위치가 타겟의 요격 최대 범위 안이라면
+        # 정수값과 실수값 차이 감안해서, 현재 인덱스가 현 타겟의 요격 범위 내라면
         if idx > target[0]-1:
-            # 현재 위치를 현위치, 타깃 요격 가능한 최대 범위 중 작은 것으로
+            # 인덱스를 현재 인덱스 값과 타깃의 최대 요격범위 직전 정수값 중 작은 것으로 설정
             idx = min(idx, target[1]-1)
-        # 현재 위치가 타겟의 요격 최소 범위에 미달한다면
+        # 만약 현 인댁스가 타겟의 최소 요격 범위에 미달한다면
         else:
-            idx = target[1]-1
+            idx = target[1] - 1
             answer += 1
-        
+            
+    
     return answer
