bd4430d0a10f0ff471bb79b363b21bb154b8b2d7
kunwoo park <scv74502@gmail.com>
2023-06-24 01:38:47 +0900

    [Silver V] Title: 집합, Time: 3844 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,44 +1,69 @@
 import sys
 
-# print(ord('A')) 65
-# print(ord('Z')) 90
-# print(ord('a')) 97
-# print(ord('z')) 122
+# # 자료구조 형식, 31256KB에 5476ms
+# def calculate(calc_order, target_set:set, calc_number):
+#     if calc_order == "add":
+#         target_set.add(calc_number)
+#     elif calc_order == "remove":
+#         target_set.discard(calc_number)
+#     elif calc_order == "check":
+#         if calc_number in target_set:
+#             print(1)
+#         else:
+#             print(0)
+#     elif calc_order == "toggle":
+#         if calc_number in target_set:
+#             target_set.remove(calc_number)
+#         else:
+#             target_set.add(calc_number)
+#     elif calc_order == "all":
+#         target_set = set([i for i in range(1, 21)])
+#     elif calc_order == "empty":
+#         target_set = set()
+#
+#     return target_set
+#
+#
+# start_set = set()
+# repeat = int(sys.stdin.readline())
+#
+# for _ in range(repeat):
+#     ipt = sys.stdin.readline().strip("\n").split(" ")
+#     if len(ipt) == 1:
+#         order, number = ipt[0], 0
+#     else:
+#         order, number = ipt[0], int(ipt[1])
+#     start_set = calculate(order, start_set, number)
+#     print(start_set)
 
 
-def calculate(calc_order, target_set:set, calc_number):
-    if calc_order == "add":
-        target_set.add(calc_number)
-    elif calc_order == "remove":
-        target_set.discard(calc_number)
-    elif calc_order == "check":
-        if calc_number in target_set:
-            print(1)
-        else:
-            print(0)
-    elif calc_order == "toggle":
-        if calc_number in target_set:
-            target_set.remove(calc_number)
-        else:
-            target_set.add(calc_number)
-    elif calc_order == "all":
-        target_set = set([i for i in range(1, 21)])
-    elif calc_order == "empty":
-        target_set = set()
+# 비트마스킹
+m = int(sys.stdin.readline())
 
-    return target_set
+bit = 0
+for _ in range(m):
+    command = sys.stdin.readline().split()
 
-
-start_set = set()
-repeat = int(sys.stdin.readline())
-
-for _ in range(repeat):
-    ipt = sys.stdin.readline().strip("\n").split(" ")
-    if len(ipt) == 1:
-        order, number = ipt[0], 0
+    if command[0] == "all":
+        bit = (1 << 20) - 1
+    elif command[0] == "empty":
+        bit = 0
     else:
-        order, number = ipt[0], int(ipt[1])
-    start_set = calculate(order, start_set, number)
-    # print(start_set)
+        op = command[0]
+        num = int(command[1]) - 1
 
-
+        # add
+        if op == 'add':
+            bit |= (1 << num)
+        # remove
+        elif op == 'remove':
+            bit &= ~(1 << num)
+        # check
+        elif op == 'check':
+            if bit & (1 << num) == 0:
+                print(0)
+            else:
+                print(1)
+        # toggle
+        elif op == 'toggle':
+            bit = bit ^ (1 << num)
