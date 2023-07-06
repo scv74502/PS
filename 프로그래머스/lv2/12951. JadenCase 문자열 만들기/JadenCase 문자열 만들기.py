67a8211650055256a672358f14a143ca821b53e2
kunwoo park <scv74502@gmail.com>
2023-07-06 17:13:01 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,11 +1,17 @@
 def solution(s):
+    # 전체 문자열을 소문자로
     splited = list(s.lower())
+    
+    # 맨 첫 글자가 알파벳이면 대문자로
     if splited[0].isalpha():
         splited[0] = splited[0].upper()
     
+    # 두 번째 글자부터 검사하면서, 공백 다음 처음으로 오는 알파벳이면 대문자로 바꾸기
     for i in range(1, len(splited)):
         if splited[i-1] == " " and splited[i].isalpha():
             splited[i] = splited[i].upper()
     
+    # 리스트를 문자열로 바꾸기
+    answer =  "".join(splited)
     
-    return "".join(splited)
+    return answer
