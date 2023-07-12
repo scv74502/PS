df85914588bdfe2c1cd67533c1d7fc835e0917dc
kunwoo park <scv74502@gmail.com>
2023-07-12 17:49:07 +0900

    [unrated] Title: 호텔 대실, Time: 1.51 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -1,12 +1,19 @@
 """
 알고리즘 : imos 알고리즘
 링크 : https://velog.io/@nkrang/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-imos-%EB%B2%95
+
+-방법-
+1. 시간에 대한 일차원 배열 생성
+2. 입장 시간에 +1, 퇴장 시간에 -1
+3. 1번의 배열의 누적합 구하기
+
+유사문제 : BOJ 3020 '개똥벌레'
 """
-CLEAR_TIME = 10
+CLEAR_TIME_10 = 10
 
 
 def time_to_min(time):
-    time_min = list(time.split(":"))
+    time_min = time.split(":")
     return int(time_min[0]) * 60 + int(time_min[1])
 
 
@@ -18,17 +25,14 @@ def solution(book_time):
         end_min = time_to_min(end)
     
         imos[start_min] += 1
-        imos[end_min + CLEAR_TIME] -= 1
+        imos[end_min + CLEAR_TIME_10] -= 1
 
     answer = 0
     # 누적합
     cumulative_sum = 0
     
-    for i in range(0, 24*60):
+    for i in range(0, 24*60+10):
         cumulative_sum += imos[i]
         answer = max(answer, cumulative_sum)
     
     return answer
-        
-    
-    
