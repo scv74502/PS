479295f13f00bb60c5368d2a4f5fe9ba7d522e4d
kunwoo park <scv74502@gmail.com>
2023-07-18 16:03:16 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,18 +1,30 @@
 """
 타겟이 되는 숫자의 리스트 범위를 충분히 크게 설정해야 한다
 ex)num이 0이고 total이 6인 경우
+
+또한 가운데 값을 찾는 방식의 다른 풀이법이 있다
 """
 
 
 def solution(num, total):
-    tgt = [i for i in range(-1000, 1001)]
-    idx = 0
-    print(sum(tgt[0:num]))
+    """
+    1. 반복문 사용하기
+    """
+#     tgt = [i for i in range(-1000, 1001)]
+#     idx = 0
     
-    while True:
-        if sum(tgt[idx:idx+num]) == total:
-            return tgt[idx:idx+num]
-        elif sum(tgt[idx:idx+num]) < total:
-            idx += 1
-        else:
-            idx -= 1
+#     while True:
+#         if sum(tgt[idx:idx+num]) == total:
+#             return tgt[idx:idx+num]
+#         elif sum(tgt[idx:idx+num]) < total:
+#             idx += 1
+#         else:
+#             idx -= 1
+
+    """
+    2. 가운뎃 값 찾기
+    """
+    center = total // num
+    start = center - (num - 1) // 2
+    answer = [start+i for i in range(num)]
+    return answer
