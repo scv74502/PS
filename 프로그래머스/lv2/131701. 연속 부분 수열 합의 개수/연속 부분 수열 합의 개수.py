debe50215f840c8d295bf2fc7d7638bf4c7f3773
kunwoo park <scv74502@gmail.com>
2023-07-21 17:45:06 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5435.70 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,6 +1,12 @@
+"""
+1. 완전 탐색 해결법
+- 집합을 이용해서 중복된 부분 수열의 합을 ㅈ거
+- 원형 수열이 n개면, 0번부터 n-1번 원소에 원소마다 1개부터 n개까지 수열을 체크
+- 시간 복잡도는 n^2 정도로 추정됨, 연산 횟수도 많이 필요함
+- dp로 개선할 필요가 있음
+"""
+
 def solution(elements):
-    answer = 0
-    dp = [0 for _ in range(1000)]
     appeared = set()
     for i in range(1, len(elements)+1):
         for j in range(0, len(elements)):
