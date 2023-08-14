441d98fbed13ec0737155a96be51048f37369004
kunwoo park <scv74502@gmail.com>
2023-08-14 17:26:01 +0900

    [Silver III] Title: N과 M (3), Time: 1412 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,20 @@
+import sys
+
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+res = []
+
+def recur(num):
+    if len(res) == M:
+        print(" ".join(map(str, res)))
+        return
+
+    for i in range(1, N+1):
+        # if i not in res:
+        res.append(i)
+        recur(i + 1)
+        res.pop()
+
+recur(1)
