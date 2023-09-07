8d1c89e06a11b7838f6afc16b9a871cc0add9143
kunwoo park <scv74502@gmail.com>
2023-09-07 15:19:13 +0900

    [Silver IV] Title: 수들의 합 2, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,27 +1,22 @@
 import sys
 
 
-""""
-투포인터 알고리즘은 자연수(양수)에서만 기능함
-구간합 m인 구간인 S와 E가 있다고 가정하자
-l이 S가 되기 전까지 r은 E를 넘어갈 수 없다
-또한 r이 E가 되기 전까지 l은 S를 넘어갈 수 없다
-"""
 input = sys.stdin.readline
-n, m = map(int, input().split(" "))
+N, M = map(int, input().split(" "))
+
 arr = list(map(int, input().split(" ")))
-r, sum, ans = 0, 0, 0
+# print(N, M, arr)
 
+end, cur, ans = 0, 0, 0
 
-for l in range(n):
-    while sum < m and r < n:
-        sum += arr[r]
-        r += 1
-
-    if sum == m:
+for start in range(N):
+    while cur < M and end < N:
+        cur += arr[end]
+        end += 1
+    if cur == M:
         ans += 1
 
-    sum -= arr[l]
-
+    cur -= arr[start]
 
 print(ans)
+
