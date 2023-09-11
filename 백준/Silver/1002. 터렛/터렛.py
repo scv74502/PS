221246bda526fad643c99a42a9868fa7e9e18a4b
kunwoo park <scv74502@gmail.com>
2023-09-12 00:57:47 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,15 +1,24 @@
+import sys
 import math
 
-n = int(input())
 
-for _ in range(n):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)  # 두 원의 거리 (원의방정식활용)
-    if distance == 0 and r1 == r2 :  # 두 원이 동심원이고 반지름이 같을 때
+input = sys.stdin.readline
+rpt = int(input())
+for _ in range(rpt):
+    j1, p1, r1, j2, p2, r2 = map(int, input().split())
+    # 원의 방정식 통해서 두 원 중심의 거리 구함
+    distance = math.sqrt((j1 - j2) ** 2 + (p1 - p2) ** 2)
+    # 중심 거리가 0이고 반지름이 같으면 같은 원, 무한대
+    if distance == 0 and r1 == r2:
         print(-1)
-    elif abs(r1-r2) == distance or r1 + r2 == distance:  # 내접, 외접일 때
+    # 두 원이 내접 혹은 외접일 때
+    elif abs(r1-r2) == distance or r1 + r2 == distance:
         print(1)
-    elif abs(r1-r2) < distance < (r1+r2) :  # 두 원이 서로다른 두 점에서 만날 때
+    # 두 원이 두 점에서 만난다면
+    elif abs(r1-r2) < distance < (r1 + r2):
         print(2)
+    # 두 원의 접점이 없다면
     else:
-        print(0)  # 그 외에
+        print(0)
+
+
