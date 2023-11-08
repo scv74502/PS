54bd37e967687fc95f81369e1f6b4d6ed4c0ee10
kunwoo park <scv74502@gmail.com>
2023-11-09 00:22:24 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 3314.93 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,14 @@
+def isPalindrome(str: str):
+    if str == str[::-1]:
+        return True
+    return False
+
+def solution(s):
+    answer = 1
+
+    for start in range(len(s)):
+        for end in range(len(s), start-1, -1):
+            if(isPalindrome(s[start: end])):
+                answer = max(answer, end - start)
+    
+    return answer
