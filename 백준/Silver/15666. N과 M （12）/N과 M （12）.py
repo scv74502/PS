bb6608cc8ac2afdf5991a05786827f2e924de11a
kunwoo park <scv74502@gmail.com>
2023-11-14 19:06:17 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,23 +1,29 @@
 import sys
 
-input = sys.stdin.readline
-N, M = map(int, input().strip().split(" "))
-nums = sorted(list(map(int, input().strip().split(" "))))
-visited = [False] * N
+
+si = sys.stdin.readline
+N, M = map(int, si().split(" "))
+nums = list(map(int, si().split(" ")))
+nums.sort()
 res = []
 
 
-def bt(start):
+
+def bt(num: int):
     if len(res) == M:
         print(" ".join(map(str, res)))
         return
-    temp = 0
-    for i in range(start, N):
-        if temp != nums[i]:
+    # backtracking 중 이전에 사용된 숫자를 저장함
+    # nums가 sorted 되어 있으므로 중복으로 숫자 추가하는 경우 피할 수 있다
+    before_used = 0
+    for i in range(num, N):
+        if before_used != nums[i]:
             res.append(nums[i])
-            temp = nums[i]
+            before_used = nums[i]
             bt(i)
             res.pop()
 
 
 bt(0)
+
+
