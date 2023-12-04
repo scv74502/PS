ab7f9299b062d19c99dc05917f09ee42840dd7ef
kunwoo park <scv74502@gmail.com>
2023-12-05 02:19:22 +0900

    pre1_3.py fix

--- books/algo_puzzle/pre1_3.py
+++ books/algo_puzzle/pre1_3.py
@@ -6,13 +6,14 @@ for i in range(0, M + 1):
     table[i] = [0] * (N + 1)
     table[i][0] = 1
 
-print(table)
 
 for i in range(1, M + 1):
     for j in range(2, N + 1):
         if (i >= 2) and (j >= i):
-            table[i][j] = table[i][j-1]
+            # print(f"{table[i][j]} = {table[i][j - i]}")
+            table[i][j] = table[i][j - i]
         if i > 2:
-            table[i][j] += table[i-1][j]
+            # print(f"{table[i][j]} += {table[i - 1][j]}")
+            table[i][j] += table[i - 1][j]
 
 print(table[M][N])
