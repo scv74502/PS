d785ac9c87cd8a7f5e541e31a402fe626946441d
kunwoo park <scv74502@gmail.com>
2023-12-16 16:08:02 +0900

    [Silver II] Title: 섬의 개수, Time: 80 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,3 +1,8 @@
+"""
+BFS 사용시, 입력 크기가 50으로 한정되어 있으므로 미리 만든 뒤에 섬 입력받음 + visited도 같은 방식으로 처리해야 시간초과 발생 안함
+함수로 넘기기, 동적으로 리스트 입력받기, visited 동적으로 생성하기 순으로 각 요소를 제거 후, 큐에 넣을지 체크하기 전 방문처리 로직 추가하니 시간 초과 미발생
+"""
+
 import sys
 from collections import deque
 
@@ -7,6 +12,20 @@ mw = [0, 0, -1, 1, -1, 1, -1, 1]
 sin = sys.stdin.readline
 map_arr = [[-1 for __ in range(51)] for _ in range(51)]
 visited = [[False for __ in range(51)] for _ in range(51)]
+sys.setrecursionlimit(2602)
+
+
+# DFS - 너비 우선 탐색을 사용함
+def dfs(height: int, width: int):
+    visited[height][width] = True
+    for d in range(8):
+        nh, nw = height + mh[d], width + mw[d]
+        if 0 <= nh < H and 0 <= nw < W:
+            if not visited[nh][nw]:
+                visited[nh][nw] = True
+                if map_arr[nh][nw] == 1:
+                    dfs(nh, nw)
+
 
 while True:
     W, H = map(int, sin().split(" "))
@@ -22,25 +41,30 @@ while True:
     cnt = 0
     dq = deque()
 
-    # 모든 점을 탐색하며, 방문여부 체크 후 너비 우선 탐색 수행
     for i in range(H):
         for j in range(W):
-            # 매 점마다 너비 우선 탐색 수행하며 체크
             if not visited[i][j] and map_arr[i][j] == 1:
-                # 탐색하지 않았던 점이라면 섬 하나 추가함
                 cnt += 1
-                dq.append([i, j])
-                visited[i][j] = True
-                while dq:
-                    ch, cw = dq.popleft()
-                    # visited[ch][cw] = True
+                dfs(i, j)
 
-                    for d in range(8):
-                        nh, nw = ch + mh[d], cw + mw[d]
-                        if 0 <= nh < H and 0 <= nw < W:
-                            if not visited[nh][nw]:
-                                visited[nh][nw] = True
-                                if map_arr[nh][nw] == 1:
-                                    dq.append([nh, nw])
+    # BFS - 모든 점을 탐색하며, 방문여부 체크 후 너비 우선 탐색 수행
+    # for i in range(H):
+    #     for j in range(W):
+    #         # 매 점마다 너비 우선 탐색 수행하며 체크
+    #         if not visited[i][j] and map_arr[i][j] == 1:
+    #             # 탐색하지 않았던 점이라면 섬 하나 추가함
+    #             cnt += 1
+    #             dq.append([i, j])
+    #             visited[i][j] = True
+    #             while dq:
+    #                 ch, cw = dq.popleft()
+    #
+    #                 for d in range(8):
+    #                     nh, nw = ch + mh[d], cw + mw[d]
+    #                     if 0 <= nh < H and 0 <= nw < W:
+    #                         if not visited[nh][nw]:
+    #                             visited[nh][nw] = True
+    #                             if map_arr[nh][nw] == 1:
+    #                                 dq.append([nh, nw])
 
     print(cnt)
