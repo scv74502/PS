405d6c11132b2a49e4a17dbfe017bace8451ea9a
kunwoo park <scv74502@gmail.com>
2023-12-16 21:34:09 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -5,13 +5,10 @@ ipt_str = sin().strip()
 
 
 def is_pel(string: str) -> int:
-    size = len(string)
-    for i in range(size, size // 2, -1):
-        if string[i - 1] != string[size - i]:
-            return 0
-            break
-
-    return 1
+    if string == string[::-1]:
+        return 1
+    else:
+        return 0
 
 
 print(is_pel(ipt_str))
