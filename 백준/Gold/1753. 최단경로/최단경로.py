ac64e3eb521a314bd88fd445b7bac6ad6b708c88
kunwoo park <scv74502@gmail.com>
2023-12-18 22:42:23 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 73684 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -17,20 +17,24 @@ for _ in range(E):
 
 def dijkstra(start: int):
     hq = []
+    # hq 초기 세팅으로 시작 정점과, 거리 0을 투입(자기 자신과의 거리는 0이므로)
     heapq.heappush(hq, [0, start])
     # 자기 자신과의 거리는 0이다
     distance[start] = 0
 
     while hq:
+        # vertex 정점에서 이전 정점까지 거리가 dist이다
         dist, vertex = heapq.heappop(hq)
         # 이미 최소 거리로 처리된 정점은 무시한다
         if distance[vertex] < dist:
             continue
-        for i in graph[vertex]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(hq, [cost, i[0]])
+        # line[0]은 vertex에서 line[1]까지의 가중치
+        for line in graph[vertex]:
+            # cost = 시작 정점에서 현 정점까지의 비용 + 현 정점에서 새 정점까지의 비용
+            cost = dist + line[1]
+            if cost < distance[line[0]]:
+                distance[line[0]] = cost
+                heapq.heappush(hq, [cost, line[0]])
 
 
 dijkstra(K)
