533db0d8d3c7bbed709767442d28f079afdbdfc4
kunwoo park <scv74502@gmail.com>
2024-01-17 16:29:53 +0900

    [Gold V] Title: 신기한 소수, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,14 +1,17 @@
 """
-맨 앞자리가 소수인 2, 3, 5, 7만 가능하다는 사실을 파악했어야 함
-맨 앞자리 제외한 나머지 자리는 홀수(자릿수 상관없이 한 자리의 2 제외한 짝수면 2로 나누어지므로)만 가능하다(1, 3, 5, 7, 9)
-그렇다면 역으로 2부터 n 자릿수까지 숫자를 붙여 나가는 방식도 가능하다!
+1. 수는 한 자리부터 8자리까지
+2. 맨 왼쪽 첫 자리는 소수인 2, 3, 5, 7만 가능
+3. 그 이후 자리들은 홀수여야 한다(1, 3, 5, 7, 9)
+4. N개의 9로 이루어진 수부터 내려가는 식으로 검사 -> 일일히 체크해야 함, 에라토스테네스 체도 메모리 제한 탓에 사용 불가함
+-> 그러나 첫 자리부터 오른쪽으로 수를 추가하다가 프린트하는 방식이면 정렬도 수행하고 따로 저장 안해도 된다
+# 두 번째 자리 수부터 붙여나갈 때, 1도 고려해야 한다(31같은 케이스 있다)
+# 소수 판별 함수 부분에서 3 이상의 홀수를 체크하는 이유는 is_prime은 두 자리수부터 동작하기 때문이다(한 자리수 소수는 체크할 일이 없어진다)
 """
+import math
 import sys
 
 
-sys.setrecursionlimit(10000)
 sin = sys.stdin.readline
-N = int(sin())
 
 
 def is_prime(num: int) -> bool:
@@ -17,27 +20,28 @@ def is_prime(num: int) -> bool:
     elif num % 2 == 0:
         return False
 
-    for i in range(3, int(num / 2) + 1, 2):
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
         if num % i == 0:
             return False
 
     return True
 
 
-def bt(num):
+def bt(num: int):
     if 10 ** (N-1) <= num < 10 ** N:
         print(num)
         return
 
+    num *= 10
     for i in range(1, 10, 2):
-        if is_prime(num * 10 + i):
-            # print("True")
-            bt(num * 10 + i)
-
-# print(10 ** (N-1), 10 ** N)
-bt(2)
-bt(3)
-bt(5)
-bt(7)
+        if is_prime(num + i):
+            bt(num + i)
 
 
+if __name__ == '__main__':
+    sys.setrecursionlimit(10000)
+    N = int(sin())
+    bt(2)
+    bt(3)
+    bt(5)
+    bt(7)
