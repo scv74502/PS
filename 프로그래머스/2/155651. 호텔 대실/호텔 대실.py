2bda0fac5c68872386adeabf64a39e382a8b4214
kunwoo park <scv74502@gmail.com>
2024-06-28 01:20:10 +0900

    [level 2] Title: 호텔 대실, Time: 14.50 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,17 +1,15 @@
-import heapq
-
-
 def convert_time(str_time):
     return int(str_time[:2])*60 + int(str_time[3:])
 
 
 def solution(book_time):
-    rooms = []
-    book_time.sort(key = lambda _:_[0])
-    for book in book_time :
-        check_in = convert_time(book[0])
-        check_out = convert_time(book[1]) + 10
-        if rooms and rooms[0] <= check_in :
-            heapq.heappop(rooms)
-        heapq.heappush(rooms,check_out)
-    return len(rooms)
+    times = dict()
+    for book in book_time:
+        start, end = convert_time(book[0]), convert_time(book[1])+10
+        for t in range(start, end):
+            if times.get(t) is None:
+                times[t] = 1
+            else:
+                times[t] += 1
+
+    return max(times.values())
