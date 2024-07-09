9a5da41daf7b9bf906537293453e2f8dbf6235a3
kunwoo park <scv74502@gmail.com>
2024-07-10 01:36:50 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18524 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -9,8 +9,8 @@ public class Main {
         int[] dp = new int[num+1];
         for(int i = 2; i <= num; i++){
             dp[i] = dp[i-1] + 1;
-            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
         }
         bw.write(dp[num]+"\n");
         bw.flush();
