7d9266e84535c11d109199ab7554a06df94a6f83
kunwoo park <scv74502@gmail.com>
2024-09-09 19:35:55 +0900

    [level 3] Title: 등굣길, Time: 0.63 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -5,38 +5,11 @@ class Solution{
         int[][] map = new int[n+1][m+1];
         int[][] dp = new int[n+1][m+1];
         
-        // for(int[] arr:dp){
-        //     Arrays.fill(arr, Integer.MAX_VALUE);
-        // }
-        
         for(int[] puddle:puddles){
             map[puddle[1]][puddle[0]] = -1;
         }                
         
         dp[1][1] = 1;                        
-        
-        
-//         for(int i = 1; i <= n; i++){
-//             for(int j = 1; j <= m; j++){
-//                 if(map[i-1][j] != -1 && map[i][j-1] == -1){
-//                     if(map[i][j] != -1){
-//                         dp[i][j] += dp[i-1][j];
-//                     }
-//                 }
-                
-//                 else if(map[i][j-1] != -1 && map[i-1][j] == -1){
-//                     if(map[i][j] != -1){
-//                         dp[i][j] += dp[i][j-1];
-//                     }
-//                 }
-                
-//                 else{
-//                     if(map[i][j] != -1){
-//                         dp[i][j] += dp[i-1][j] + dp[i][j-1];
-//                     }
-//                 }
-//             }
-//         }
         
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
@@ -46,7 +19,6 @@ class Solution{
             }
         }
         
-        // System.out.println(Arrays.deepToString(dp));
         return dp[n][m];
     }
 }
