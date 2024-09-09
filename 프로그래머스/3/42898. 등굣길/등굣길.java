5c70f621ff6553782dd96d1d305a311f1427668f
kunwoo park <scv74502@gmail.com>
2024-09-09 19:47:32 +0900

    [level 3] Title: 등굣길, Time: 0.76 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -13,6 +13,7 @@ class Solution{
         
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
+                if(i == 1 && j == 1) continue;
                 if(map[i][j] != -1){
                     dp[i][j] += (dp[i-1][j] + dp[i][j-1])  % 1000000007;
                 }
