469029128eb61c8846689686b2b2fe9d574878a7
kunwoo park <scv74502@gmail.com>
2024-10-23 11:25:30 +0900

    [level 2] Title: 미로 탈출, Time: 18.91 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 16.38 ms
+메모리: 10.2 MB, 시간: 18.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 01:41:18
+2024년 10월 23일 11:25:26
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,50 +1,59 @@
 from collections import deque
 
 
-def bfs(start_y, start_x, end_y, end_x, maps):
+def bfs(start_r, start_c, end_r, end_c, maps):
+    row = len(maps)
+    col = len(maps[0])
+    visited = [[False for _ in range(col)] for __ in range(row)]
+    
     # 상 하 좌 우
-    dy = [-1, 1, 0, 0]
-    dx = [0, 0, -1, 1]
-    # 세로, 가로
-    N, M = len(maps), len(maps[0])
-    visited = [[False for _ in range(M)] for __ in range(N)]
+    mr = [-1, 1, 0, 0]
+    mc = [0, 0, -1, 1]
+    
     dq = deque()
-    dq.append([start_y, start_x, 0])
-
+    dq.append([start_r, start_c, 0])
+    visited[start_r][start_c] = True
+    
     while dq:
-        cy, cx, cd = dq.popleft()
-        if cy == end_y and cx == end_x:
+        cr, cc, cd = dq.popleft()
+        if cr == end_r and cc == end_c:
             return cd
-        for m in range(4):
-            ny, nx = cy + dy[m], cx + dx[m]
-            if 0 <= ny < N and 0 <= nx < M and maps[ny][nx] != 'X':
-                if not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    dq.append([ny, nx, cd + 1])
-
+        
+        for mv in range(4):
+            nr, nc = cr + mr[mv], cc + mc[mv]
+            if nr < 0 or row <= nr or nc < 0 or col <= nc:
+                continue
+            if maps[nr][nc] != 'X' and not visited[nr][nc]:
+                visited[nr][nc] = True
+                dq.append([nr, nc, cd + 1])
     return -1
 
-
 def solution(maps):
-    n, m = len(maps), len(maps[0])
     answer = 0
-    # 시작, 레버, 종착지의 x, y 좌표
-    sy, sx, ly, lx, ey, ex = 0, 0, 0, 0, 0, 0
-    for i in range(n):
-        for j in range(m):
+    row = len(maps)
+    col = len(maps[0])
+    startRow, startCol, leverRow, leverCol, endRow, endCol = 0, 0, 0, 0, 0, 0
+    
+    for i in range(0, row):
+        for j in range(0, col):
             if maps[i][j] == 'S':
-                sy, sx = i, j
-            elif maps[i][j] == 'E':
-                ey, ex = i, j
-            elif maps[i][j] == 'L':
-                ly, lx = i, j
-
-    answer += bfs(sy, sx, ly, lx, maps)
-    if answer > -1:
-        temp = bfs(ly, lx, ey, ex, maps)
-        if temp == -1:
-            answer = temp
-        else:
+                startRow, startCol = i, j
+            if maps[i][j] == 'L':
+                leverRow, leverCol = i, j
+            if maps[i][j] == 'E':
+                endRow, endCol = i, j
+    # print(startRow, startCol, leverRow, leverCol, endRow, endCol)
+    
+    answer = bfs(startRow, startCol, leverRow, leverCol, maps)
+    # print(answer)
+    if answer > -1:        
+        temp = bfs(leverRow, leverCol, endRow, endCol, maps)    
+        if temp > -1:
             answer += temp
+        else:
+            answer = temp
+    return answer
+            
+                
     
     return answer
