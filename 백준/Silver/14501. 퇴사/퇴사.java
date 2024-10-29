a87e19bccc9d1aeebdd8726e0bd715553d78cb34
kunwoo park <scv74502@gmail.com>
2024-10-30 01:07:47 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,39 +1,42 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] ipt;
-        int[][] tp; // time and price
-        int rpt = Integer.parseInt(br.readLine());
-        tp = new int[rpt + 1][2];
-        int[] dp = new int[rpt +1];
+        String[] ipts;
+        String ipt;
 
-        for(int r = 1; r <= rpt; r++){
-            ipt = br.readLine().split(" ");
-            tp[r][0] = Integer.parseInt(ipt[0]);
-            tp[r][1] = Integer.parseInt(ipt[1]);
+        int N = Integer.parseInt(br.readLine());
+        int[][] schedule = new int[N][2];
 
-            dp[r] = tp[r][1];
+        for(int i = 0; i < N; i++) {
+            ipts = br.readLine().split(" ");
+            int consultDay = Integer.parseInt(ipts[0]);
+            int consultPrice = Integer.parseInt(ipts[1]);
+            schedule[i][0] = consultDay;
+            schedule[i][1] = consultPrice;
         }
 
-        for(int i = 2; i <= rpt; i++){
-            for(int j = 1; j < i; j++){
-                if(i - j >= tp[j][0]) { // 남아 있는 날에 상담을 추가할 수 있다면
-                    // dp[i] = max(dp[i], tp[i][1] + dp[j], 현재까지 최대 값과 추가상담비 + 전까지 상담비 비교하기
-                    dp[i] = Math.max(dp[i], tp[i][1] + dp[j]);
-                }
+        int[] dp = new int[N+1];
+
+        // 첫 날부터 진행, bottom-up
+        for(int i = 0; i < N; i++) {
+            int consultDay = schedule[i][0];
+            int consultPrice = schedule[i][1];
+
+            // i번 상담을 끝낸 날
+            int consultEndDay = i + consultDay;
+            
+            // 상담 끝낸 날부터 마지막 날까지
+            for (int j = consultEndDay; j <= N; j++) {
+                // 처음에는 비어 있으므로 i번째 날짜에 상담 진행한 금액이 들어옴
+                // 그 후에 다른 값들중 큰 값으로 갱신됨
+                dp[j] = Math.max(dp[j], dp[i] + consultPrice);
             }
         }
-
-        int max = 0;
-        for(int i = 1; i <= rpt; i++){
-           if(i + tp[i][0] <= rpt + 1){
-               if(max < dp[i]) max = dp[i];
-           }
-        }
-
-        System.out.println(max);
+        
+        System.out.println(dp[N]);
     }
 }
