dcf674582afa4b497b535fb90503a4de9b795bb8
kunwoo park <scv74502@gmail.com>
2024-10-30 16:28:39 +0900

    [Gold V] Title: 퇴사 2, Time: 888 ms, Memory: 340544 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -21,19 +21,21 @@ public class Main {
         // dp[i] -> i일까지밖에 없을 대 최대값
         int[] dp = new int[N+1];
 
-        // 첫 날부터 진행, bottom-up
-        for(int i = 1; i <= N; i++) {
-            // 이전까지의 최대값
+        for(int i = 1; i <= N; i++){
+            // 이전까지의 최대값, i일자 상담을 진행하지 않더라도 i-1일자의 최대값은 i일에도 최대값이다
             dp[i] = Math.max(dp[i], dp[i-1]);
-            int consultDay = schedule[i-1][0];
-            int consultPrice = schedule[i-1][1];
 
-            // 해당 상담을 진행한 마지막 날, 당일을 포함한다
-            int finDay = i + consultDay - 1;
+            // 상담의 마지막 날은 현재날짜 + 상담소요시간 - 1
+            // -1 없으면 상담이 끝난 첫날
+            int consultEndDay = i + schedule[i-1][0] - 1; // i일 상담의 마지막 날
+            int curPrice = schedule[i-1][1];  // i일 진행하는 상담의 수임료
 
-            if(finDay <= N){
-                dp[finDay] = Math.max(dp[finDay], dp[i-1] + consultPrice);
-            }
+            // N일자 넘어서 끝나는 상담은 진행하지 않음
+            if(consultEndDay > N) continue;
+
+            // dp[i]는 i일까지 상담 진행했을 때의 최대금액
+            // 오늘 기준으로 어제까지 상담 최대금액 + 오늘 진행하는 상담료를 오늘 상담 마지막날의 최대 상담료와 비교하여 큰 값을 대입함
+            dp[consultEndDay] = Math.max(dp[consultEndDay], dp[i-1] + curPrice);
         }
 
         System.out.println(dp[N]);
