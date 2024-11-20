93ad9348c9ce67271ad32c4ea1bdeaf0dc8705a3
kunwoo park <scv74502@gmail.com>
2024-11-20 15:33:06 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 17384 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,57 +1,48 @@
 import java.io.*;
-import java.util.Arrays;
 
-public class Main {
-    public static int K, N;
-    // cable은 모두 int 범위 내의 수이다
-    public static int[] cables;
+public class Main{
+    static int K, N;
+    static long[] wires;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] ipts = br.readLine().split(" ");
+        String[] ipts;
+        String ipt;
+
+        ipts = br.readLine().split(" ");
         K = Integer.parseInt(ipts[0]);
         N = Integer.parseInt(ipts[1]);
-        cables = new int[K];
-
-        for(int i = 0; i < K; i++){
-            cables[i] = Integer.parseInt(br.readLine());
-        }
-        // 여기까지 문제의 입력 파트
-        // 이후로 케이블을 정렬하고 이분 탐색으로 탐색
-
-        long answer = 1L;
+        wires = new long[K];
 
         long maxCable = 0;
-        for(int i = 0; i < K; i++){
-            maxCable = Math.max(maxCable, cables[i]);
+
+        for (int i = 0; i < K; i++) {
+            wires[i] = Long.parseLong(br.readLine());
+            maxCable = Math.max(maxCable, wires[i]);
         }
 
-        maxCable++;
-
-        // 랜선을 못 민드는 가장
-        answer = bisect(0, maxCable);
+        long answer = bisect(0, maxCable+1);
         System.out.println(answer - 1);
     }
 
-    public static long bisect(long left, long right){
+    static long bisect(long left, long right){
         long mid = 0;
-        while(left < right){
+        while (left < right) {
             mid = (left + right) / 2;
-            if(getWireAmount(mid) < N){
+            if(getWiresAmount(mid) < N){
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-
         return left;
     }
 
-    public static long getWireAmount(long size){
-        long answer = 0;
-        for(int i = 0; i < K; i++){
-            answer += cables[i] / size;
+    static long getWiresAmount(long wireLen){
+        long ans = 0;
+        for(long wire:wires){
+            ans += wire / wireLen;
         }
-        return answer;
+        return ans;
     }
 }
