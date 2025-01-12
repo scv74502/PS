2e09e0c9340f1e4d2ee9725b02fb6ec89f9b3ebf
kunwoo park <scv74502@gmail.com>
2025-01-12 20:37:40 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 492 ms, Memory: 114372 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,41 +1,45 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] numbers;
+    static int[] arr;
     static int[] lis;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        numbers = new int[N];
-        String[] ipts = br.readLine().split(" ");
+        String[] ipts;
 
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(ipts[i]);
-        }
-
+        int N = Integer.parseInt(br.readLine());
+        ipts = br.readLine().split(" ");
+        arr = new int[N];
         lis = new int[N + 1];
 
-        int lisLen = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(ipts[i]);
+        }
+
+        // LIS의 가장 큰 값이 있는 위치, LIS의 현재 길이
         int lisIdx = 0;
+        int lisLen = 0;
 
         for (int i = 0; i < N; i++) {
-            if(numbers[i] > lis[lisLen]){
+            // lis의 가장 큰 값이 현재 숫자보다 작으면 큰 값으로 갱신
+            if(arr[i] > lis[lisLen]){
                 lisLen++;
-                lis[lisLen] = numbers[i];
-            } else {
-                lisIdx = binarySearch(0, lisLen, numbers[i]);
-                lis[lisIdx] = numbers[i];
+                lis[lisLen] = arr[i];
+            } else{
+                // lis의 기존 값들 중에서 갱신하기
+                lisIdx = bisect(0, lisLen, arr[i]);
+                lis[lisIdx] = arr[i];
             }
         }
 
         System.out.println(lisLen);
     }
 
-    public static int binarySearch(int left, int right, int target){
-        int mid = 0;
+    // lis 중에서 target보다 작거나 같은 상한선 위치 이분 탐색으로 탐색
+    public static int bisect(int left, int right, int target){
+        int mid;
+
         while(left < right){
             mid = (left + right) / 2;
 
