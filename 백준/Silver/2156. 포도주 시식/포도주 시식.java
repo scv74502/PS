158682726af43553ea1367286c22d6124a9ef230
kunwoo park <scv74502@gmail.com>
2025-03-29 00:48:25 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,37 +1,38 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] ipts;
 
         int N = Integer.parseInt(br.readLine());
-        int[] wines = new int[N];
-        int[] dp = new int[N + 1];
-
-        for(int i = 0; i < N; i++){
+        int[] wines = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             wines[i] = Integer.parseInt(br.readLine());
         }
 
-        int answer = 0;
+        int[] dp = new int[N + 1];
+
         if(N < 3){
-            for(int i = 0; i < N; i++){
-                answer += wines[i];
+            int sum = 0;
+            for (int i = 1; i <= N; i++) {
+                sum += wines[i];
             }
-        } else{
-            dp[1] = wines[0];
-            dp[2] = dp[1] + wines[1];
-            for(int i = 3; i <= N; i++){
-                // 3잔 기준으로 점화식을 세운다
-                dp[i] = dp[i-3] + wines[i-1] + wines[i-2];    // 둘째잔, 셋째잔 마심
-                dp[i] = Math.max(dp[i], dp[i-2] + wines[i-1]);    // 첫째잔, 지금잔 마심
-                dp[i] = Math.max(dp[i], dp[i-1]);    // 첫째잔, 둘째잔 마심
-            }
-            answer = dp[N];
+            System.out.println(sum);
+            return;
         }
 
-        System.out.println(answer);
+        for (int i = 1; i <= 2; i++) {
+            dp[i] = dp[i - 1] + wines[i];
+        }
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 3] + wines[i-1] +wines[i]);
+            dp[i] = Math.max(dp[i], dp[i - 2] + wines[i]);
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+        }
+
+        System.out.println(dp[N]);
     }
 }
