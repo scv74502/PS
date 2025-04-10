eba6d01c6308fea7d2592ae8894f90f1f461b667
kunwoo park <scv74502@gmail.com>
2025-04-10 18:09:55 +0900

    [Silver I] Title: 스티커, Time: 644 ms, Memory: 110652 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,55 +1,46 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[][] stickers;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         int tc = Integer.parseInt(br.readLine());
 
         for (int repeat = 0; repeat < tc; repeat++) {
             int N = Integer.parseInt(br.readLine());
             stickers = new int[2][N];
+            dp = new int[2][N];
 
             for (int i = 0; i < 2; i++) {
-                String[] ipts = br.readLine().split(" ");
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    stickers[i][j] = Integer.parseInt(ipts[j]);
+                    stickers[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            int[][] dp = new int[2][N];
+            dp[0][0] = stickers[0][0];
+            dp[1][0] = stickers[1][0];
 
-            if(N == 1){
-                sb.append(Math.max(stickers[0][0], stickers[1][0]));
-                sb.append("\n");
-                continue;
-            } else if (N == 2) {
-                sb.append(Math.max(stickers[0][0] + stickers[1][1], stickers[1][0] + stickers[0][1]));
-                sb.append("\n");
-            } else{
-                dp[0][0] = stickers[0][0];
-                dp[1][0] = stickers[1][0];
-
-                dp[0][1] = dp[1][0] + stickers[0][1];
-                dp[1][1] = dp[0][0] + stickers[1][1];
-
-                for (int i = 2; i < N; i++) {
-                    dp[0][i] = Math.max(dp[1][i-1] + stickers[0][i], dp[1][i-2] + stickers[0][i]);
-                    dp[1][i] = Math.max(dp[0][i-1] + stickers[1][i], dp[0][i-2] + stickers[1][i]);
-                }
-
-                sb.append(Math.max(dp[0][N-1], dp[1][N-1]));
-                sb.append("\n");
+            if(N > 1){
+                dp[0][1] = stickers[1][0] + stickers[0][1];
+                dp[1][1] = stickers[0][0] + stickers[1][1];
             }
+
+            int answer = -1;
+
+            // 전 칸 대각선 스티커를 떼는 경우
+            for (int i = 2; i < N; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1] + stickers[0][i], dp[1][i - 2] + stickers[0][i]);
+                dp[1][i] = Math.max(dp[0][i - 1] + stickers[1][i], dp[0][i - 2] + stickers[1][i]);
+            }
+
+            answer = Math.max(dp[0][N-1], dp[1][N-1]);
+
+            System.out.println(answer);
         }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
