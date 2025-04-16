fe167533559c567c15217744b6e2d23cc20cdb14
kunwoo park <scv74502@gmail.com>
2025-04-16 17:35:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 184 ms, Memory: 53644 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -5,35 +5,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] ipts;
-        String ipt;
 
         ipts = br.readLine().split(" ");
         int N = Integer.parseInt(ipts[0]);
         int K = Integer.parseInt(ipts[1]);
 
-        int[] weights = new int[N+1];
-        int[] values = new int[N+1];
+        int[] productWeights = new int[N];
+        int[] productValues = new int[N];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             ipts = br.readLine().split(" ");
-            // 무게와 가치
-            weights[i] = Integer.parseInt(ipts[0]);
-            values[i] = Integer.parseInt(ipts[1]);
+            productWeights[i] = Integer.parseInt(ipts[0]);
+            productValues[i] = Integer.parseInt(ipts[1]);
         }
 
+        // N개를 선택하며 K 무게만큼 담는 최대 가치
         int[][] dp = new int[N+1][K+1];
+        int answer = 0;
 
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
+            int weight = productWeights[i-1];
+            int value = productValues[i-1];
+
             for (int j = 1; j <= K; j++) {
-                // i번 물건을 넣지 않는 경우에는, i-1번째 최대 무게를 가져옴
                 dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
 
-                if(j >= weights[i]) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - weights[i]] + values[i]);
-                }
+                if(j >= weight){
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - weight] + value);
+                    answer = Math.max(answer, dp[i][j]);                }
             }
         }
 
-        System.out.println(dp[N][K]);
+        System.out.println(answer);
     }
 }
