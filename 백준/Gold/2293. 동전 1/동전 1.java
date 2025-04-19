b07eda2a53bd79bd03042253e7b31bf5579c0e7b
kunwoo park <scv74502@gmail.com>
2025-04-19 22:46:01 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,38 +1,24 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
         int[] coins = new int[N];
-
         for (int i = 0; i < N; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
-
-        int[][] dp = new int[N+1][K+1];
-        dp[0][0] = 1;   // 0원을 만드는 법은 1가지
-
-        for (int i = 1; i <= N; i++) {
-            int coin = coins[i-1];  // 현재 동전의 금액
-            dp[i][0] = 1;
-
-            for (int j = 0; j <= K; j++) {
-                dp[i][j] = dp[i-1][j];  // i개째 동전을 고르지 않더라도 i-1개로 만든 j원 가짓수는 유지된다
-
-                if(j >= coin){
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j] + dp[i][j-coin]);
-                }
+        int[] dp = new int[K+1];
+        dp[0] = 1;   // 0원을 만드는 법은 1가지
+        
+        for (int i = 0; i < N; i++) {
+            int coin = coins[i];  // 현재 동전의 금액
+            for (int j = coin; j <= K; j++) {
+                dp[j] += dp[j-coin];  // 현재 동전을 사용하여 j원을 만드는 경우의 수 추가
             }
         }
-
-        System.out.println(dp[N][K]);
+        System.out.println(dp[K]);
     }
 }
