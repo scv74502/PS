e31a972f20cac18b886bdc460820f0c28aa6b8cf
kunwoo park <scv74502@gmail.com>
2025-04-22 12:05:06 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,5 +1,6 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,21 +12,22 @@ public class Main {
 
         int[] coins = new int[n];
         int[] dp = new int[k + 1];
-        Arrays.fill(dp, k + 1);
-        dp[0] = 0;
+
+        Arrays.fill(dp, 100001);
 
         for (int i = 0; i < n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(coins);
+        dp[0] = 0;
 
-        for (int coin:coins) {
+        for (int i = 0; i < n; i++) {
+            int coin = coins[i];
             for (int j = coin; j <= k; j++) {
-                dp[j] = Math.min(dp[j - coin] + 1, dp[j]);
+                dp[j]  = Math.min(dp[j] ,dp[j - coin] + 1);
             }
         }
 
-        System.out.println(dp[k] == k+1 ? -1 : dp[k]);
+        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
     }
 }
