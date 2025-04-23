9500c037c6fab416c1442ceb552b58d0747ca0af
kunwoo park <scv74502@gmail.com>
2025-04-23 17:27:21 +0900

    [Gold IV] Title: 동전 1, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,24 +1,31 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+
         int[] coins = new int[N];
+
         for (int i = 0; i < N; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
+
         int[] dp = new int[K+1];
-        dp[0] = 1;   // 0원을 만드는 법은 1가지
-        
-        for (int i = 0; i < N; i++) {
-            int coin = coins[i];  // 현재 동전의 금액
-            for (int j = coin; j <= K; j++) {
-                dp[j] += dp[j-coin];  // 현재 동전을 사용하여 j원을 만드는 경우의 수 추가
+        dp[0] = 1;
+
+        for (int coin:coins){
+            for (int i = coin; i <= K; i++) {
+                dp[i] += dp[i-coin];
             }
         }
+
         System.out.println(dp[K]);
     }
 }
