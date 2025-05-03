7de236f773272802133531965d0bbedd99e74513
kunwoo park <scv74502@gmail.com>
2025-05-03 17:49:34 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 44.01 ms, Memory: 182 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] solution(int[][] edges) {
+        int VERTEX_COUNT = -1;
+        
+        for(int[] edge:edges){
+            VERTEX_COUNT = Math.max(VERTEX_COUNT, edge[0]);
+            VERTEX_COUNT = Math.max(VERTEX_COUNT, edge[1]);
+        }
+        
+        int[] inCnt = new int[VERTEX_COUNT + 1];
+        int[] outCnt = new int[VERTEX_COUNT + 1];
+        
+        for(int[] edge:edges){
+            int u = edge[0];
+            int v = edge[1];
+            inCnt[v]++;
+            outCnt[u]++;
+        }
+        
+        // System.out.println(VERTEX_COUNT);
+        
+        int[] answer = {0, 0, 0, 0};
+        
+        for(int i = 1; i <= VERTEX_COUNT; i++){
+            if(inCnt[i] == 0 && outCnt[i] >= 2){    // 생성 노드
+                answer[0] = i;
+            } 
+            
+            else if (inCnt[i] >= 1 && outCnt[i] == 0){  // 막대 그래프
+                answer[2] += 1;
+            } 
+            
+            else if (inCnt[i] >= 2 && outCnt[i] == 2){  // 막대 그래프
+                answer[3] += 1;
+            } 
+        }
+        
+        answer[1] = outCnt[answer[0]] - (answer[2] + answer[3]);
+        
+        return answer;
+    }
+}
