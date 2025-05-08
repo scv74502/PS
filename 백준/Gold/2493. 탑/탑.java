e5aa96f94eee24adf8cbfed6c629d011d65ddfd8
kunwoo park <scv74502@gmail.com>
2025-05-08 17:41:57 +0900

    [Gold V] Title: 탑, Time: 812 ms, Memory: 90524 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,28 +6,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 
         Stack<int[]> stack = new Stack<>();
 
+        // i번째 수라는 뜻
         for (int i = 1; i <= N; i++) {
-            int newNum = Integer.parseInt(st.nextToken());
+            int newNumber = Integer.parseInt(stringTokenizer.nextToken());
             while(!stack.isEmpty()){
-                if(newNum <= stack.peek()[0]){
-                    sb.append(stack.peek()[1] + " ");
+                if(stack.peek()[0] >= newNumber){    // 레이저 발사하여 닿는 범위라면
+                    // 현 탑에서 레이저 쏘면 닿음, 해당 탑 정보 출력
+                    sb.append(stack.peek()[1]);
+                    sb.append(" ");
                     break;
                 }
                 stack.pop();
             }
 
+            // 스택이 비었으면 현 탑보다 높아서 레이저 닿는 곳이 없으므로 0 출력
             if(stack.isEmpty()){
-                sb.append(0 + " ");
+                sb.append("0");
+                sb.append(" ");
             }
-            stack.push(new int[]{newNum, i});
+
+            stack.push(new int[] {newNumber, i});
         }
 
         bw.write(sb.toString());
