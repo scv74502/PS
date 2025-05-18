7bcf4b4f9a59d06ed8c2ba478054c8f1bb618bb9
kunwoo park <scv74502@gmail.com>
2025-05-18 22:09:21 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 124 ms, Memory: 14940 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int tc = Integer.parseInt(br.readLine());
+        int[][] dp;
+
+        dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= 10000; i++) {
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+        }
+
+        for (int test = 0; test < tc; test++) {
+            int N = Integer.parseInt(br.readLine());
+            int answer = 0;
+            for (int i = 1; i <= 3; i++) {
+                answer += dp[N][i];
+            }
+
+            System.out.println(answer);
+        }
+    }
+}
