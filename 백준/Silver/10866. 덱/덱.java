f0bdacf07ae4cd82ec29dbbf9b7f089f56b62497
kunwoo park <scv74502@gmail.com>
2025-06-05 15:28:02 +0900

    [Silver IV] Title: 덱, Time: 284 ms, Memory: 20184 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] ipts = br.readLine().split(" ");
+
+            if(ipts[0].equals("push_front")){
+                int num = Integer.parseInt(ipts[1]);
+                queue.offerFirst(num);
+            } else  if(ipts[0].equals("push_back")){
+                int num = Integer.parseInt(ipts[1]);
+                queue.offer(num);
+            } else if(ipts[0].equals("pop_front")){
+                if(queue.isEmpty()){
+                    System.out.println(-1);
+                } else {
+                    System.out.println(queue.pollFirst());
+                }
+            } else if(ipts[0].equals("pop_back")){
+                if(queue.isEmpty()){
+                    System.out.println(-1);
+                } else {
+                    System.out.println(queue.pollLast());
+                }
+            } else if(ipts[0].equals("size")){
+                System.out.println(queue.size());
+            } else if(ipts[0].equals("empty")){
+                System.out.println(queue.isEmpty() ? 1 : 0);
+            } else if(ipts[0].equals("front")){
+                if(queue.isEmpty()){
+                    System.out.println(-1);
+                } else {
+                    System.out.println(queue.peekFirst());
+                }
+            } else if(ipts[0].equals("back")){
+                if(queue.isEmpty()){
+                    System.out.println(-1);
+                } else {
+                    System.out.println(queue.peekLast());
+                }
+            } else { continue;}
+        }
+
+    }
+}
