107ae8652c8fc95e8a61eea85bc86a39d81119ee
kunwoo park <scv74502@gmail.com>
2025-06-15 22:06:30 +0900

    [level 2] Title: 기능개발, Time: 1.43 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,44 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] leftday = new int[speeds.length];
-        // 줄 서는 자료구조인 큐를 통해 첫날의 남은 일수부터 주입함
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> ans = new ArrayList<Integer>();
-        for(int i = 0; i < speeds.length; i++){
-            int progress = progresses[i];
-            int day = 0;
-            
-            while(progress < 100){
-                day += 1;
-                progress += speeds[i];
-            }
-            leftday[i] = day;
-            queue.offer(day);
-        }
-        // System.out.println(Arrays.toString(leftday));
+        Queue<int[]> queue = new ArrayDeque<>();
         
-        int tmp = 0;
-        int count = 0;
+        for(int i = 0; i < progresses.length; i++){
+            queue.add(new int[] {progresses[i], i});
+        }
+        
+        ArrayList<Integer> deployedPerDay = new ArrayList<>();
         
         while(!queue.isEmpty()){
-            // 가장 최근 날자를 확인하여 현재 남은 일수와 비교하여
-            if(tmp > queue.peek()){
-                queue.poll();
-            } else{
-                tmp = queue.poll();
+            int rpt = queue.size();                       
+            for(int i = 0; i < rpt; i++){
+                int[] curProgress = queue.poll();
+                curProgress[0] += speeds[curProgress[1]];                                                
+                queue.add(curProgress);
             }
-            count++;
             
-            // 모든 일자를 검사한 후의 마지막 값이라면
-            if(queue.isEmpty()){
-                ans.add(count);
-            } else if (queue.peek()> tmp){
-                ans.add(count);
-                count = 0;
-                tmp = 0;
+            int deployed = 0;
+            
+            while(!queue.isEmpty() && queue.peek()[0] >= 100) {
+                queue.poll();
+                deployed++;
             }
+            if(deployed > 0) deployedPerDay.add(deployed);
+        }                        
+        
+        
+        // System.out.println(deployedPerDay);
+        
+        int[] answer = new int[deployedPerDay.size()];
+        
+        for(int i = 0; i < deployedPerDay.size(); i++){
+            answer[i] = deployedPerDay.get(i);
         }
-        return ans.stream().mapToInt(i->i).toArray();
+        
+        return answer;
     }
 }
