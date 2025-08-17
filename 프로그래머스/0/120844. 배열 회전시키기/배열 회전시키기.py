7eebe3f98db34457ef3f764dd24e556b419e4bd6
kunwoo park <scv74502@gmail.com>
2025-08-17 15:18:15 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,12 +1,16 @@
-from collections import deque
-
 def solution(numbers, direction):
-    dq = deque(numbers)
     
     if direction == "left":
-        dq.append(dq.popleft())
+        num = numbers[0]
+        
+        for i in range(0, len(numbers)-1):
+            numbers[i] = numbers[i+1]
+        numbers[-1] = num
     else:
-        dq.appendleft(dq.pop())
+        num = numbers[-1]
+        
+        for i in range(len(numbers)-1, 0, -1):
+            numbers[i] = numbers[i-1]
+        numbers[0] = num
     
-    answer = list(dq)
-    return answer
+    return numbers
