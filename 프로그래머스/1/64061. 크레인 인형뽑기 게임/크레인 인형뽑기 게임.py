200f398a6e31020ac1a031f255aee582bf1699d1
kunwoo park <scv74502@gmail.com>
2025-08-18 23:14:33 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.61 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,31 +1,27 @@
 def solution(board, moves):
-    N = len(board)
-    moves = [move-1 for move in moves]
+    n = len(board)
+    stack = [[] for _ in range(n)]
+    
+    for i in range(n-1, -1, -1):
+        line = board[i]
+        for i, num in enumerate(line):
+            if num == 0:
+                continue
+            stack[i].append(num)
+    
     basket = []
     answer = 0
     
     for move in moves:
-        idx, cur = 0, 0
-        
-        while cur == 0 and idx < N:
-            cur = board[idx][move]
-            idx += 1
-        
-        # 맨 밑바닥까지 도달했는데도 아무것도 못 집으면 아무 일도 안 일어남
-        if cur == 0:
+        cur_idx = move - 1
+        if not stack[cur_idx]:
             continue
-        
-        board[idx-1][move] = 0
-        
-        # print(basket, cur)
-        
-        if basket and cur == basket[-1]:
-            basket.append(cur)
-            for _ in range(2):
-                answer += 1
-                basket.pop()
+        elif basket and stack[cur_idx][-1] == basket[-1]:
+            stack[cur_idx].pop()
+            basket.pop()
+            answer += 2
         else:
-            basket.append(cur)
-        
-        
+            basket.append(stack[cur_idx].pop())
+    
+    
     return answer
