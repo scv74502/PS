f5230bcbfad0e5043849a0a5ba22108c163a62df
kunwoo park <scv74502@gmail.com>
2025-10-11 16:15:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 63.24 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -2,36 +2,54 @@ from collections import deque
 
 
 def solution(n, wires):
-    res = 100
-    graph = [[] for _ in range(n)]
+    answer = n
+    
+    graph = [[False for _ in range(n+1)] for __ in range(n+1)]
+    # 그래프 선 연결
+    for u, v in wires:
+        graph[u][v] = True
+        graph[v][u] = True
+    
+    for u, v in wires:
+        graph[u][v] = False
+        graph[v][u] = False
+        
+        answer = min(answer, search(n, graph))
+        
+        graph[u][v] = True
+        graph[v][u] = True
+    
+    return answer
 
-    for i, j in wires:
-        graph[i - 1].append(j - 1)
-        graph[j - 1].append(i - 1)
 
-    def bfs(start: int):
-        visited = [False for _ in range(n)]
-        dq = deque()
-        dq.append(start)
-        cnt = 0
+def search(n, graph):
+    visited = [False for _ in range(n+1)]
+    results = []
+    for i in range(1, n+1):
+        if visited[i]:
+            continue
+        result = bfs(i, graph, visited)
+        results.append(result)
+        
+    return abs(results[0] - results[1])
 
-        while dq:
-            cnt += 1
-            cur = dq.popleft()
-            visited[cur] = True
-            for node in graph[cur]:
-                if not visited[node]:
-                    dq.append(node)
-
-        return cnt
-
-    for i, j in wires:
-        graph[i - 1].remove(j - 1)
-        graph[j - 1].remove(i - 1)
-
-        res = min(res, abs(bfs(i - 1) - bfs(j - 1)))
-
-        graph[i - 1].append(j - 1)
-        graph[j - 1].append(i - 1)
-
-    return res
+def bfs(vertex, graph, visited):
+    n = len(graph) - 1
+    visited[vertex] = True
+    answer = 1
+    
+    dq = deque()
+    dq.append(vertex)
+    
+    while dq:
+        cur_vertex = dq.pop()
+        for i in range(1, n+1):
+            if i == cur_vertex or not graph[cur_vertex][i] or visited[i]:
+                continue
+            dq.append(i)
+            visited[i] = True
+            answer += 1
+    
+    return answer
+    
+    
