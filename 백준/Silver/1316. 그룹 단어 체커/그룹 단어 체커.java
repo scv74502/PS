3c1e681d66cb29c56653c85c1bf4f888a1c8dd7a
kunwoo park <scv74502@gmail.com>
2026-01-19 11:22:18 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,40 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        int rpt = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int N = Integer.parseInt(br.readLine());
         int answer = 0;
-        boolean flag;
 
-         for(int r = 0; r < rpt; r++) {
-             flag = true;
-             String word = br.readLine();
-             int left = 0;
-             int right = 0;
-             Set<Character> store = new HashSet<>();
-             while(right < word.length()) {
-                 char ch = word.charAt(left);
-                 if(store.contains(ch)) {
-                     flag = false;
-                     break;
-                 }
-                 store.add(ch);
-                 while(right < word.length() && word.charAt(right) == ch) {
-                     right += 1;
-                 }
-                 left = right;
-             }
-             if(flag) {
-                 answer += 1;
-             }
-         }
-         sb.append(answer);
-         bw.write(sb.toString());
-         bw.flush();
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            if(isGroupWord(word)){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static boolean isGroupWord(String word){
+        Set<Character> set = new HashSet<>();
+        char prev = ' ';
+        for(char ch:word.toCharArray()){
+            if(set.contains(ch) && ch != prev){
+                return false;
+            } else{
+                set.add(ch);
+            }
+
+            prev = ch;
+        }
+        return true;
     }
 }
