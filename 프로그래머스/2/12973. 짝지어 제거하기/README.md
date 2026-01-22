b1035bc891b9e5328edb86322b50a45ca5eda622
kunwoo park <scv74502@gmail.com>
2026-01-22 11:29:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 89.40 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 25.4 MB, 시간: 181.87 ms
+메모리: 11.5 MB, 시간: 89.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 26일 18:57:52
+2026년 01월 22일 11:29:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,20 +1,10 @@
 def solution(s):
-    stack1 = list(s)[::-1]
-    stack2 = []
+    stack = []
     
-    while stack1:        
-        if stack1 and stack2 and stack1[-1] == stack2[-1]:
-            stack1.pop()
-            stack2.pop()
-            continue
+    for ch in s:
+        if stack and stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
         
-        stack2.append(stack1.pop())
-        if stack1 and stack1[-1] == stack2[-1]:
-            stack1.pop()
-            stack2.pop()
-        # print(stack1, stack2)
-    
-    if not stack1 and not stack2:
-        return 1
-        
-    return 0
+    return int(not stack)
