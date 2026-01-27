dc025cafddc9b2bdbf49ea843a1e6ba78882248b
kunwoo park <scv74502@gmail.com>
2026-01-27 13:22:04 +0900

    [level 2] Title: 할인 행사, Time: 50.42 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 45.98 ms
+메모리: 130 MB, 시간: 50.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 19:07:35
+2026년 01월 27일 13:22:00
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,62 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
+        int THRESHOLD = 10;
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(int i = 0; i < THRESHOLD; i++){
+            if(!hm.containsKey(discount[i])) hm.put(discount[i], 1);
+            else hm.put(discount[i], hm.get(discount[i]) + 1);
+        }
+        
+        HashMap<String, Integer> target = new HashMap<>();
+        for(int i = 0; i < want.length; i++){
+            target.put(want[i], number[i]);
+        }
+        
+        int joinDay = 1;
         int answer = 0;
-
-            HashMap<String, Integer> target = new HashMap<>();
-            HashMap<String, Integer> current = new HashMap<>();
-
-            int numberSum = 0;
-            for(int num: number){
-                numberSum += num;
-            }
-
-            int left = 0;
-            boolean found = true;
-            int right = numberSum-1;
-
-            for (int i = 0; i < want.length; i++) {
-                target.put(want[i], number[i]);
-            }
-
-            for (int i = 0; i < numberSum; i++) {
-                if(!current.containsKey(discount[i])){
-                    current.put(discount[i], 1);
-                } else{
-                    current.put(discount[i], current.get(discount[i]) + 1);
-                }
-            }
-
-
-
-            while(right < discount.length) {
-                for(String key:target.keySet()){
-                    if(!current.containsKey(key)){
-                        found = false;
-                        break;
-                    } else if (current.get(key) < target.get(key)) {
-                        found = false;
-                        break;
-                    }
-                }
-                if(found){
-                    answer += 1;
-                }
-
-
-                current.put(discount[left], current.get(discount[left]) - 1);
-                left++;
-                right++;
-                if(right == discount.length){ break; }
-                if(!current.containsKey(discount[right])){
-                    current.put(discount[right], 1);
-                } else {
-                    current.put(discount[right], current.get(discount[right]) + 1);
-                }
-                found = true;
-            }
-
-
-            return answer;
+        
+        // 첫날 검사
+        if(check(hm, target)) answer++;
+        
+        while(joinDay + THRESHOLD <= discount.length){            
+            hm.put(discount[joinDay - 1], hm.get(discount[joinDay - 1]) - 1);            
+            if(!hm.containsKey(discount[THRESHOLD + joinDay - 1])) hm.put(discount[THRESHOLD + joinDay - 1], 1);
+            else hm.put(discount[THRESHOLD + joinDay - 1], hm.get(discount[THRESHOLD + joinDay - 1]) + 1);
+            
+            joinDay++;
+            
+            if(check(hm, target)) answer++;                        
+        }
+        
+        return answer;
+    }
+    
+    public boolean check(HashMap<String, Integer> hm, HashMap<String, Integer> target){        
+        for(String key: target.keySet()){
+            if(!hm.containsKey(key) || hm.get(key) != target.get(key)) return false;
+        }
+        
+        return true;
     }
 }
