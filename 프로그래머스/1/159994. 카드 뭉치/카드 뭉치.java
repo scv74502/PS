c82108940e4c9415464cb8aed16ec3b3c42a6f3a
kunwoo park <scv74502@gmail.com>
2026-01-27 10:52:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Queue<Integer> cards1Queue = new LinkedList<>();
-        Queue<Integer> cards2Queue = new LinkedList<>();
+        Queue<String> cards1Queue = new ArrayDeque<>();
+        Queue<String> cards2Queue = new ArrayDeque<>();
         
         for(int i = 0; i < cards1.length; i++){
-            cards1Queue.add(i);
+            cards1Queue.add(cards1[i]);
         }
         
         for(int i = 0; i < cards2.length; i++){
-            cards2Queue.add(i);
+            cards2Queue.add(cards2[i]);
         }
         
         int goalIdx = 0;
         
         while(goalIdx < goal.length){
-            if(!cards1Queue.isEmpty() && cards1[cards1Queue.peek()].equals(goal[goalIdx])){
+            if(!cards1Queue.isEmpty() && cards1Queue.peek().equals(goal[goalIdx])){
                 cards1Queue.poll();
-            } else if(!cards2Queue.isEmpty() && cards2[cards2Queue.peek()].equals(goal[goalIdx])){
+            } else if(!cards2Queue.isEmpty() && cards2Queue.peek().equals(goal[goalIdx])){
                 cards2Queue.poll();
             } else{
                 return "No";
