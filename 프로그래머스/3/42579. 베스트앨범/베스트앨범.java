8a08bab78edb078d75a177c0df3e2c7a42de4a37
kunwoo park <scv74502@gmail.com>
2026-01-29 10:54:42 +0900

    [level 3] Title: 베스트앨범, Time: 1.41 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,73 +2,61 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> playtimeByGenre = new HashMap<>();
-        HashMap<String, List<SongInfo>> songByGenre = new HashMap<>();
-        ArrayList<SongInfo> songlist = new ArrayList<>();
+        int N = plays.length;
         
+        HashMap<String, Integer> playPerGenre = new HashMap<>();
+        HashMap<String, ArrayList<SongInfo>> songPerGenre = new HashMap<>();
+        
+        // 장르별 재생시간, 장르별 곡 목록 정리하기
         for(int i = 0; i < plays.length; i++){
-            String genre = genres[i];
-            int play = plays[i];
-            
-            if(!playtimeByGenre.containsKey(genre)){
-                playtimeByGenre.put(genre, play);
-            } else {
-                playtimeByGenre.put(genre, playtimeByGenre.get(genre) + play);
-            }
-        
-            if(!songByGenre.containsKey(genre)){
-                songByGenre.put(genre, new ArrayList<>());
-                songByGenre.get(genre).add(new SongInfo(i, play));
+            if(!playPerGenre.containsKey(genres[i])){
+                playPerGenre.put(genres[i], plays[i]);
             } else{
-                songByGenre.get(genre).add(new SongInfo(i, play));
+                playPerGenre.put(genres[i], playPerGenre.get(genres[i]) + plays[i]);
             }
-                        
+            
+            if(!songPerGenre.containsKey(genres[i])){
+                songPerGenre.put(genres[i], new ArrayList<>());
+            }
+            
+            songPerGenre.get(genres[i]).add(new SongInfo(i, plays[i]));
         }
-
-        // 장르 이름들만 뽑아서 리스트로
-        List<String> genreList = new ArrayList<>(playtimeByGenre.keySet());
-
-        // 재생시간 내림차순 정렬(큰게 앞으로)
-        Collections.sort(genreList, (o1, o2) -> 
-            playtimeByGenre.get(o2) - playtimeByGenre.get(o1)
-        );
+        
+        ArrayList<String> genreList = new ArrayList<>(playPerGenre.keySet());
+        Collections.sort(genreList, (o1, o2) -> playPerGenre.get(o2) - playPerGenre.get(o1));
         
         ArrayList<Integer> resultList = new ArrayList<>();
 
-        for(String genre : genreList){ // 정렬된 장르 리스트를 순회
-            List<SongInfo> songs = songByGenre.get(genre);
-            Collections.sort(songs); // 노래 정렬 (재생수 내림차순, 번호 오름차순)
-
-            // 최대 2개까지만 리스트에 담기
+        for(String genre:genreList){
+            ArrayList<SongInfo> songs = songPerGenre.get(genre);
+            Collections.sort(songs);
             resultList.add(songs.get(0).no);
-            if(songs.size() > 1){
-                resultList.add(songs.get(1).no);
-            }
-        }
-
-        // 리스트를 배열로 변환
+            
+            if(songs.size() > 1) resultList.add(songs.get(1).no);
+        }               
+        
         int[] answer = new int[resultList.size()];
-        for(int i=0; i<resultList.size(); i++){
+        for(int i = 0; i < resultList.size(); i++){
             answer[i] = resultList.get(i);
         }
         return answer;
     }
 }
 
-class SongInfo implements Comparable<SongInfo> {    
+class SongInfo implements Comparable<SongInfo> {
     int no;
-    int playtime;
+    int play;
     
-    public SongInfo(int no, int playtime){        
+    public SongInfo(int no, int play){
         this.no = no;
-        this.playtime = playtime;
+        this.play = play;
     }
     
     @Override
-    public int compareTo(SongInfo si){
-        if(si.playtime == this.playtime){
-            return this.no - si.no;
+    public int compareTo(SongInfo songInfo){
+        if(this.play == songInfo.play){
+            return this.no - songInfo.no; // 고유변호 오름차순(작은 것 먼저)
         }
-        return si.playtime - this.playtime;
-    }    
+        return songInfo.play - this.play;   // 재생 내림차순(큰 것 먼저)
+    }
 }
