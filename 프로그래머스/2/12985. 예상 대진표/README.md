8104dbb78ec11e7f8d9af81b0f7451b72705eb05
kunwoo park <scv74502@gmail.com>
2026-02-04 10:34:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
-메모리: 81.4 MB, 시간: 0.03 ms
+메모리: 76 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 03일 14:22:36
+2026년 02월 04일 10:34:03
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,17 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        if(n == 2) return 1;
-        int answer = 0;                
+        int answer = 0;
         
-        // a와 b가 같아지면 경기에서 만난다는 의미
-        while (a != b) {
-            // (번호 + 1) / 2를 하면 다음 라운드 번호가 됩니다.            
+        while(a != b) {
             a = (a + 1) / 2;
             b = (b + 1) / 2;
             answer++;
         }
         
+
         return answer;
     }
 }
