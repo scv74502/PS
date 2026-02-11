4508966df6cba88a895a3b9685ce896d09f3c51d
kunwoo park <scv74502@gmail.com>
2026-02-11 11:41:10 +0900

    [level 3] Title: 양과 늑대, Time: 54.62 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 8.02 ms
+메모리: 85.2 MB, 시간: 54.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 10일 11:48:52
+2026년 02월 11일 11:41:07
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -4,65 +4,72 @@ class Solution {
     public int solution(int[] info, int[][] edges) {
         int answer = 0;
         
-        ArrayList<Integer>[] graph = new ArrayList[info.length];
+        ArrayList<Integer>[] graph = new ArrayList[info.length];        
+        
         for(int i = 0; i < info.length; i++){
-            graph[i] = new ArrayList<Integer>();
+            graph[i] = new ArrayList<>();
         }
         
-        for(int[] edge:edges){
+        for(int[] edge: edges){
             graph[edge[0]].add(edge[1]);
-        }
-                
+        }                
         
-        Deque<Info> queue = new ArrayDeque<>();
-        queue.add(new Info(0, 1, 0, new HashSet<>()));
+        Deque<Info> deque = new ArrayDeque<>();
+        deque.add(new Info(0, 1, 0, new HashSet<>()));  // 시작은 0부터
         
-        while(!queue.isEmpty()){            
-            int curLoc = queue.peek().curLoc;
-            int curSheep = queue.peek().sheep;
-            int curWolf = queue.peek().wolf;
-            HashSet<Integer> visited = queue.peek().visited;
-            queue.poll();                        
+        // visited는 최근 방문 기록, 중복체크가 아닌 다음 노드 탐색할 위치
+        while(!deque.isEmpty()){
+            System.out.println(deque.peek());
+            int curLoc = deque.peek().location;
+            int curSheep = deque.peek().sheepCnt;
+            int curWolf = deque.peek().wolfCnt;
             answer = Math.max(answer, curSheep);
+            HashSet<Integer> curVisited = new HashSet<>(deque.peek().visited);
+            deque.poll();
             
-            // 인접 노드에 업데이트
-            for(int nextLoc: graph[curLoc]){
-                visited.add(nextLoc);
+            // 현재 마지막 방문지역의 하위 노드 방문처리 
+            for(int childLoc: graph[curLoc]){
+                curVisited.add(childLoc);
             }
-                                    
-            for(int nextLoc: visited){                
-                int nextSheep = curSheep;
-                int nextWolf = curWolf;                
-                HashSet<Integer> nextVisited = new HashSet<>(visited);
+            
+            // 마지막 방문지역으로부터 현재 방문할 노드들 탐색
+            for(int nextLoc: curVisited){
+                if(info[nextLoc] == 0){ // 다음 노드에 양이 있다면
+                    HashSet<Integer> nextVisited = new HashSet<>(curVisited);
+                    nextVisited.remove(nextLoc);
+                    deque.add(new Info(nextLoc, curSheep + 1, curWolf, nextVisited));
+                }
                 
-                // 양이 있는 칸
-                if(info[nextLoc] == 0){
-                    nextVisited.remove(nextLoc);                
-                    queue.add(new Info(nextLoc, nextSheep + 1, nextWolf, nextVisited));
-                } else {    // 늑대칸   
+                if(info[nextLoc] == 1){ // 다음 노드에 늑대가 있다면
                     if(curSheep > curWolf + 1){
-                        nextVisited.remove(nextLoc);                
-                        queue.add(new Info(nextLoc, nextSheep, nextWolf + 1, nextVisited));
+                        HashSet<Integer> nextVisited = new HashSet<>(curVisited);
+                        nextVisited.remove(nextLoc);
+                        deque.add(new Info(nextLoc, curSheep, curWolf + 1, nextVisited));
                     }
-                }                
+                }
             }
         }
+        
         
         return answer;
     }
-    
 }
 
-class Info {
-    int curLoc;
+class Info{
+    int location;
+    int sheepCnt;
+    int wolfCnt;
     HashSet<Integer> visited;
-    int sheep;
-    int wolf;
     
-    public Info(int curLoc, int sheep, int wolf, HashSet<Integer> visited){
-        this.curLoc = curLoc;        
-        this.sheep = sheep;
-        this.wolf = wolf;
+    public Info(int location, int sheepCnt, int wolfCnt, HashSet<Integer> visited){
+        this.location = location;
+        this.sheepCnt = sheepCnt;
+        this.wolfCnt = wolfCnt;
         this.visited = visited;
     }
+    
+    // @Override
+    // public String toString(){
+    //     return "location: " + location + ", sheelCnt: " + sheepCnt + ", wolfCnt: " + wolfCnt + "\nvisited: " + visited + "\n";
+    // }
 }
