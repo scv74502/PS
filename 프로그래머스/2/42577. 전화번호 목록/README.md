21645fff707b6c79d550ebbac07d501312e47b19
kunwoo park <scv74502@gmail.com>
2026-02-13 10:42:37 +0900

    [level 2] Title: 전화번호 목록, Time: 674.53 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
 
 ### 성능 요약
 
-메모리: 243 MB, 시간: 1054.32 ms
+메모리: 145 MB, 시간: 674.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 21일 00:53:29
+2026년 02월 13일 10:42:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,33 +1,24 @@
 import java.util.*;
 
-
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book, new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2){
-                return o1.length() - o2.length();
-            }
-        });        
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
-        boolean answer = true;
-        for(String pn:phone_book){
-            hm.put(pn, 1);
+        Arrays.sort(phone_book, Comparator.comparingInt(String::length).reversed()
+                                  .thenComparing(Comparator.naturalOrder()));
+        
+        Set<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        for(String pNum: phone_book){
+            set.add(pNum);
         }
         
-        for(String pn:phone_book){
-            //System.out.println("pn : " + pn);
-            String temp = "";
-            for(int p = 0; p < pn.length(); p++){
-                //System.out.println("temp : " + temp);
-                //System.out.println("ss : " + pn.substring(p, p+1));
-                temp += pn.substring(p, p+1);
-                if(hm.containsKey(temp) && !temp.equals(pn)){
-                    //System.out.println("return temp : " + temp);
-                    return false;
-                }
+        for(String pNum: phone_book){
+            for(int i = 0; i < pNum.length() - 1; i++){
+                sb.append(pNum.charAt(i));
+                if (set.contains(sb.toString())) return false;                
             }
+            sb.setLength(0);
         }
-        return answer;
+        
+        return true;
     }
 }
