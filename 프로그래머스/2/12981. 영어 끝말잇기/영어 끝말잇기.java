30c4aefe56467292bcbdf122b28e2b4e110baacb
kunwoo park <scv74502@gmail.com>
2026-02-13 10:16:33 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,35 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int idx = 0;
-        HashSet<String> checkedWord = new HashSet<>();
-        char firstChar = ' ';
-        
-        while(idx < words.length){             
-            String curWord = words[idx];
-            System.out.println(idx + ", curWord: " + curWord);
-            // 처음 등장 단어면 통과
-            if(idx == 0) {
-                firstChar = curWord.charAt(curWord.length() - 1);
-                checkedWord.add(curWord);
-                idx++;
-                continue;
+        Set<String> usedWords = new HashSet<>();
+        char lastChar = words[0].charAt(0); // 첫 단어 통과를 위해 첫 글자로 초기화
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            // 탈락 조건: 끝말이 이어지지 않거나 이미 사용된 단어인 경우
+            if (word.charAt(0) != lastChar || usedWords.contains(word)) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
             }
-            
-            // 다음 단어로 넘어가기 전 검사
-            if(curWord.charAt(0) != firstChar || checkedWord.contains(curWord)) {
-                System.out.println("defeated! -> " + idx + ", curWord: " + curWord);
-                int defeatedUserIdx = (idx) % n + 1;
-                int defeatedTurnIdx = idx / n + 1;
-                return new int[] {defeatedUserIdx, defeatedTurnIdx};
-            }
-            
-            idx++;                        
-            
-            firstChar = curWord.charAt(curWord.length() - 1);
-            checkedWord.add(curWord);                        
+
+            usedWords.add(word);
+            lastChar = word.charAt(word.length() - 1);
         }
 
-        return new int[] {0, 0};
+        return new int[]{0, 0};
     }
 }
