911ece56edd21896999d6e8404d5bebf0b208e73
kunwoo park <scv74502@gmail.com>
2026-02-13 10:58:40 +0900

    [level 2] Title: 전화번호 목록, Time: 365.55 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,23 +2,16 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book, Comparator.comparingInt(String::length).reversed()
-                                  .thenComparing(Comparator.naturalOrder()));
-        
-        Set<String> set = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
-        for(String pNum: phone_book){
-            set.add(pNum);
-        }
-        
-        for(String pNum: phone_book){
-            for(int i = 0; i < pNum.length() - 1; i++){
-                sb.append(pNum.charAt(i));
-                if (set.contains(sb.toString())) return false;                
+        // 사전순 정렬, 문자열 오름차순 - 길이 오름차순
+        Arrays.sort(phone_book);        
+
+        // 인접한 두 번호만 비교 
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {  // 사전순 정렬로 인해 접두어는 바로 뒤에 위치함
+                return false;
             }
-            sb.setLength(0);
         }
-        
+
         return true;
     }
 }
