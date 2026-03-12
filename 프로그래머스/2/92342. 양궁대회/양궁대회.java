320d3ad41afd0e7a82dc0140b1b4b3712f9cf3b3
kunwoo park <scv74502@gmail.com>
2026-03-12 11:13:32 +0900

    [level 2] Title: 양궁대회, Time: 24.61 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,59 +1,57 @@
-import java.util.*;
-
 class Solution {
     int maxDiff = 0;
-    int[] answer = new int[11];
+    int[] answer;
     public int[] solution(int n, int[] info) {
         int[] lionRecord = new int[11];
+        answer = new int[11];
         bt(n, 0, lionRecord, info);
-        // System.out.println(Arrays.toString(answer));
         return maxDiff == 0 ? new int[] {-1} : answer;
     }
     
     public void bt(int leftArrow, int idx, int[] lionRecord, int[] apeachRecord){
         if(idx == 11){
             lionRecord[10] += leftArrow;
-            int curDiff = calcDiff(lionRecord, apeachRecord);
+            int diff = calcDiff(lionRecord, apeachRecord);
             
-            if(maxDiff < curDiff){
-                answer = lionRecord.clone();
-                maxDiff = curDiff;                
-            } else if(maxDiff == curDiff){
-                for(int i = 10; i >= 0; i--){
-                    if(lionRecord[i] > answer[i]){
-                        answer = lionRecord.clone();
+            if(diff > maxDiff){
+                maxDiff = diff;
+                answer = lionRecord.clone();                                    
+                lionRecord[10] -= leftArrow;
+                return;
+            } else if(diff == maxDiff){
+                // 낮은 점수가 많은 쪽
+                for(int i = 10; i >= 0; i--){                    
+                    if(lionRecord[i] == 0 && answer[i] == 0) continue;
+                    else if(lionRecord[i] > answer[i]){
+                        answer = lionRecord.clone();                
                         lionRecord[10] -= leftArrow;
                         return;
-                    } else if(lionRecord[i] < answer[i]) {
-                        break;
                     } else{
-                        continue;
+                        lionRecord[10] -= leftArrow;
+                        return;
                     }
                 }
             }
             
             lionRecord[10] -= leftArrow;
             return;
-        }
+        }        
         
         for(int i = 0; i <= leftArrow; i++){
             lionRecord[idx] += i;
-            bt(leftArrow - i, idx + 1, lionRecord, apeachRecord);            
+            bt(leftArrow - i, idx + 1, lionRecord, apeachRecord);
             lionRecord[idx] -= i;
         }
     }
     
     public int calcDiff(int[] lionRecord, int[] apeachRecord){
-        int answer = 0;
+        int lion = 0;
+        int apeach = 0;
         for(int i = 10; i >= 0; i--){
-            if(lionRecord[i] == 0 && apeachRecord[i] == 0){
-                continue;
-            } else if(lionRecord[i] > apeachRecord[i]){
-                answer += (10 - i);
-            } else{
-                answer -= (10 - i);
-            }
+            if(lionRecord[i] == 0 && apeachRecord[i] == 0) continue;
+            if(lionRecord[i] > apeachRecord[i]) lion += 10 - i;
+            else apeach += 10 - i;            
         }
-        return answer;
+        return lion - apeach;
     }
 }
