3a141d6b973b8b1c141e5bbf8b6693c16d15dd7c
kunwoo park <scv74502@gmail.com>
2026-03-31 10:40:08 +0900

    [Gold V] Title: 톱니바퀴, Time: 112 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,16 +1,12 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    // i번 톱니바퀴의 j번 톱니 값
     static int[][] cogWheels = new int[4][8];
-    // i번 톱니바퀴가 다른 톱니와 닿아 있는 값들(0번 인덱스가 왼쪽, 1번 인덱스가 오른쪽)
-    static int[][] cogStatus = new int[4][2];
+    static int[][] wheelStatus = new int[4][2]; // i번 휠의 왼쪽 접점 6번 인덱스, 오른쪽 접점 2번 인덱스
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 톱니바퀴 4개 입력
         for (int i = 0; i < 4; i++) {
             String ipt = br.readLine();
             char[] numbers = ipt.toCharArray();
@@ -18,67 +14,57 @@ public class Main {
                 cogWheels[i][j] = numbers[j] - '0';
             }
 
-            cogStatus[i][0] = 6;
-            cogStatus[i][1] = 2;
+            wheelStatus[i][0] = 6;
+            wheelStatus[i][1] = 2;
         }
 
         int K = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < K; i++) {
-            String[] iptArray = br.readLine().split(" ");
-            int cogWheelIdx = Integer.parseInt(iptArray[0]) - 1;
-            int rotateDir = Integer.parseInt(iptArray[1]);
+            String[] iptArr = br.readLine().split(" ");
+            int cogWheelIdx = Integer.parseInt(iptArr[0]) - 1;
+            int rotateDir = Integer.parseInt(iptArr[1]);
+
             rotate(cogWheelIdx, rotateDir);
         }
 
         int answer = 0;
-
         for (int i = 0; i < 4; i++) {
-            int topIdx = (cogStatus[i][0] + 2) % 8;
-            if (cogWheels[i][topIdx] == 1) {
-                answer += (1 << i);
-            }
+            if(cogWheels[i][calcCog(wheelStatus[i][1], -2)] == 1) answer += (1 << i);
         }
 
         System.out.println(answer);
     }
 
-    // 회전 함수, rotateDir이 1이면 시계방향 -1이면 반시계방향
-    public static void rotate(int cogWheelIdx, int rotateDir) {
-        int[] directions = new int[4];
-        directions[cogWheelIdx] = rotateDir;
+    static void rotate(int idx, int rotateDir){
+        int[] dir = new int[4];
+        dir[idx] = rotateDir;
 
-        // 회전 방향 먼저 모두 결정 (회전 전 상태 기준 비교)
-        // 왼쪽 전파
-        for (int i = cogWheelIdx; i > 0; i--) {
-            if (cogWheels[i - 1][cogStatus[i - 1][1]] != cogWheels[i][cogStatus[i][0]]) {
-                directions[i - 1] = -directions[i];
-            } else break;
-        }
-        // 오른쪽 전파
-        for (int i = cogWheelIdx; i < 3; i++) {
-            if (cogWheels[i][cogStatus[i][1]] != cogWheels[i + 1][cogStatus[i + 1][0]]) {
-                directions[i + 1] = -directions[i];
-            } else break;
+        // 회전 왼쪽으로 전파
+        for(int i = idx; i > 0; i--){
+            if(cogWheels[i][wheelStatus[i][0]] == cogWheels[i-1][wheelStatus[i-1][1]]) break;
+            dir[i-1] = -dir[i];
         }
 
-        // 결정된 방향으로 인덱스 일괄 업데이트
+        // 회전 오른쪽으로 전파
+        for(int i = idx; i < 3; i++){
+            if(cogWheels[i][wheelStatus[i][1]] == cogWheels[i+1][wheelStatus[i+1][0]]) break;
+            dir[i+1] = -dir[i];
+        }
+
+        // 결정된 방향으로 회전하여 인덱스 일괄 업데이트
         for (int i = 0; i < 4; i++) {
-            if (directions[i] == 0) continue;
-            // 시계 방향(1)이면 인덱스 -1, 반시계(-1)면 인덱스 +1
-            cogStatus[i][0] = calcCog(cogStatus[i][0], -directions[i]);
-            cogStatus[i][1] = calcCog(cogStatus[i][1], -directions[i]);
+            if (dir[i] == 0) continue;
+            // 시계 방향으로 톱니를 돌리면, 실제 인덱스는 그 반대로(반시계도 마찬가지)
+            wheelStatus[i][0] = calcCog(wheelStatus[i][0], -dir[i]);
+            wheelStatus[i][1] = calcCog(wheelStatus[i][1], -dir[i]);
         }
     }
 
-    // 톱니 계산 함수
     public static int calcCog(int cog, int dir){
-        int result = cog + dir;
-
-        if(result > 7) result -= 8;
-        else if(result < 0) result += 8;
-
-        return result;
+        cog += dir;
+        if(cog < 0) return cog + 8;
+        return cog % 8;
     }
 }
 
