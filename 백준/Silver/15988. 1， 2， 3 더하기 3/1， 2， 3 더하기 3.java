cbe349c4246c1777240a16bc90863b3323513af7
kunwoo park <scv74502@gmail.com>
2026-04-16 10:42:09 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 140 ms, Memory: 22892 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int rpt = Integer.parseInt(br.readLine());
+        final int DIVIDER = 1_000_000_009;
+        long[] dp = new long[1_000_001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % DIVIDER;
+        }
+
+        for (int i = 0; i < rpt; i++) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(dp[N]);
+        }
+    }
+}
