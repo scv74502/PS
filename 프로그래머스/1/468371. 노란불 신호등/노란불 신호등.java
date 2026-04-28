2bb4cee79e23a824d20fd32070fcf75d05a9d807
kunwoo park <scv74502@gmail.com>
2026-04-28 10:57:15 +0900

    [level 1] Title: 노란불 신호등, Time: 76.90 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,34 +1,52 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] signals) {
-        int timeUpperBound = 1;
-        int[] cycle = new int[signals.length];
+        int N = signals.length;
         
-        for(int i = 0; i < signals.length; i++){            
-            int summary = 0;
-            for(int time: signals[i]){
-                summary += time;
-            }
-            cycle[i] = summary;
-            timeUpperBound *= summary;
-        }
+        // 각각 몇 초마다 사이클이 생기는지, 현재 색깔 정보, 몇 초에 어떤 색깔인지 정보
+        int[] cycleInfo = new int[N];
+        int[] curStatus = new int[N];
+        ArrayList<Integer>[] colorInfo = new ArrayList[N];    // 0이 초록색, 1이 노란색, 2가 빨간색        
         
-        for(int curTime = 1; curTime <= timeUpperBound; curTime++){
-            boolean isAllYellow = true;
+        for(int i = 0; i < N; i++){
+            colorInfo[i] = new ArrayList<>();
+            colorInfo[i].add(-1);
             
-            for(int i = 0; i < signals.length; i++){
-                int modTime = (curTime - 1) % cycle[i];
-                int greenBound = signals[i][0];
-                int yellowBound = signals[i][0] + signals[i][1];
-                
-                if(!(greenBound <= modTime && modTime < yellowBound)){
-                    isAllYellow = false;
-                    break;
+            // 현재 신호등 R, G, B
+            int[] curArr = signals[i];            
+            int sum = 0;            
+            for(int j = 0; j < curArr.length; j++) {                
+                sum += curArr[j];    
+                for(int k = 0; k < curArr[j]; k++){
+                    colorInfo[i].add(j);
                 }
             }
-            
-            if(isAllYellow) return curTime;
+            cycleInfo[i] = sum;                                    
         }
-        System.out.println(timeUpperBound);
+        
+        // System.out.println(Arrays.toString(colorInfo));
+        int maxTime = 1;
+        for(int time:cycleInfo){
+            maxTime *= time;
+        }
+        
+        for(int curTime = 1; curTime <= maxTime; curTime++){
+            for(int i = 0; i < N; i++){
+                int curColor = colorInfo[i].get(curTime % cycleInfo[i]);
+                curStatus[i] = curColor;
+            }
+                        
+            boolean allSameColor = true;
+            for(int i = 1; i < N; i++){                
+                if(curStatus[i-1] != curStatus[i]){
+                    allSameColor = false;
+                    break;
+                }
+            }                        
+            if(allSameColor && curStatus[0] == 1) return curTime;
+        }
+                
         return -1;
     }
 }
