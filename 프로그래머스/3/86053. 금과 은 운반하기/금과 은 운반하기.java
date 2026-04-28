b6f0124ff77a3ca5c4814832597bef62df444a06
kunwoo park <scv74502@gmail.com>
2026-04-28 11:32:07 +0900

    [level 3] Title: 금과 은 운반하기, Time: 135.54 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -5,43 +5,43 @@ class Solution {
     }
     
     public long bisect(int a, int b, int[] g, int[] s, int[] w, int[] t) {
+        final long MAX_RANGE = 1000000000000000L;
         long left = 0;
-        long right = 500_000_000_000_000L;
-        
-        long answer = Long.MAX_VALUE;
+        long right = MAX_RANGE;
+        long result = MAX_RANGE;
         
         while(left < right){
-            long mid = (left + right) / 2;            
-            
-            if(isAvailable(mid, a, b, g, s, w, t)) {                
+            long mid = (left + right) / 2;
+            if(isAvailable(mid, a, b, g, s, w, t)){
+                result = mid;
                 right = mid;
-            } else {
+            } else{
                 left = mid + 1;
             }
         }
-        return left;
+        
+        return result;
     }
     
-    public boolean isAvailable(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        long totalGold = 0;
+    public boolean isAvailable(long time, int a, int b, int[] g, int[] s, int[] w, int[] t){
+        // 각 마을에서 가져오는 은 총합, 금 총합, 혼합 총합
         long totalSilver = 0;
-        long totalCombined = 0;
-
-        for (int i = 0; i < g.length; i++) {            
-            long moveCount = time / (t[i] * 2L);
+        long totalGold = 0;
+        long totalCombine = 0;        
+                    
+        for(int i = 0; i < g.length; i++){
+            // 시간 내 전달(왕복) 가능한 횟수 구하기
+            long moveCnt = time / (t[i] * 2);
+            if(time % (t[i] * 2) >= t[i]) moveCnt++;
             
-            // 마지막에 편도로 끝나는 경우
-            if (time % (t[i] * 2L) >= t[i]) {
-                moveCount++;
-            }
+            long maxPayLoad = moveCnt * w[i];
             
-            long maxPayload = moveCount * w[i];
-            
-            totalGold += Math.min(g[i], maxPayload);
-            totalSilver += Math.min(s[i], maxPayload);
-            totalCombined += Math.min(g[i] + s[i], maxPayload);
-        }
+            totalSilver += Math.min(maxPayLoad, s[i]);
+            totalGold += Math.min(maxPayLoad, g[i]);
+            totalCombine += Math.min(maxPayLoad, s[i] + g[i]);
+        }                
         
-        return totalGold >= a && totalSilver >= b && totalCombined >= (long)a + b;            
+        if(totalGold >= a && totalSilver >= b && totalCombine >= (a + b)) return true;
+        return false;
     }
 }
