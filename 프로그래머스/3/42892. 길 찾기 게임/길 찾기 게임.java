a3ef08beab825309287bbc9b0d6b479d0290abb6
kunwoo park <scv74502@gmail.com>
2026-05-08 11:32:22 +0900

    [level 3] Title: 길 찾기 게임, Time: 20.35 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -2,87 +2,83 @@ import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] nodeinfo) {
-        int[][] nodeArr = new int[nodeinfo.length][3];
-        for(int i = 0; i < nodeinfo.length; i++) {
-            nodeArr[i][0] = nodeinfo[i][0];
-            nodeArr[i][1] = nodeinfo[i][1];
-            nodeArr[i][2] = i + 1;
+        // x, y, 노드번호 순으로 재배치 후 정렬
+        int[][] nodes = new int[nodeinfo.length][3];
+        for(int i = 0; i < nodeinfo.length; i++){
+            nodes[i][0] = nodeinfo[i][0];
+            nodes[i][1] = nodeinfo[i][1];
+            nodes[i][2] = i + 1;
         }
         
-        Arrays.sort(nodeArr, (a, b) -> {
+        Arrays.sort(nodes, (a, b) -> {
             if(a[1] == b[1]){
                 return a[0] - b[0];
             }
             return b[1] - a[1];
-        });        
+        });
         
-        Node root = buildTree(nodeArr);
-        ArrayList<Integer> preOrderResult = new ArrayList<>();
-        ArrayList<Integer> postOrderResult = new ArrayList<>();
-        preOrder(root, preOrderResult);
-        postOrder(root, postOrderResult);
-        int[][] answer = new int[2][nodeinfo.length];
-        
-        for(int i = 0; i < preOrderResult.size(); i++){
-            answer[0][i] = preOrderResult.get(i);
+        // 루트 노드 생성 및 노드 삽입
+        Node root = new Node(nodes[0][0], nodes[0][1], nodes[0][2]);
+        for(int i = 1; i < nodes.length; i++){
+            insertNode(root, nodes[i][0], nodes[i][1], nodes[i][2]);
         }
         
-        for(int i = 0; i < postOrderResult.size(); i++){
-            answer[1][i] = postOrderResult.get(i);
+        ArrayList<Integer> prefixResult = new ArrayList<>();
+        ArrayList<Integer> postfixResult = new ArrayList<>();
+        prefix(root, prefixResult);
+        postfix(root, postfixResult);
+        
+        int[][] answer = new int[2][nodes.length];
+        for(int i = 0; i < nodes.length; i++) {
+            answer[0][i] = prefixResult.get(i);
+        }
+        for(int i = 0; i < nodes.length; i++) {
+            answer[1][i] = postfixResult.get(i);
         }
         
         return answer;
     }
     
-    // 전위 순회 (Root -> Left -> Right)
-    public void preOrder(Node current, List<Integer> list) {
-        if (current == null) return;
-        list.add(current.nodeNumber);
-        preOrder(current.leftNode, list);
-        preOrder(current.rightNode, list);
-    }
-
-    // 후위 순회 (Left -> Right -> Root)
-    public void postOrder(Node current, List<Integer> list) {
-        if (current == null) return;
-        postOrder(current.leftNode, list);
-        postOrder(current.rightNode, list);
-        list.add(current.nodeNumber);
+    public void prefix(Node root, ArrayList<Integer> resultList) {
+        resultList.add(root.nodeNumber);
+        if(root.leftNode != null) prefix(root.leftNode, resultList);
+        if(root.rightNode != null) prefix(root.rightNode, resultList);
     }
     
-    public Node buildTree(int[][] nodeArr) {
-        Node root = new Node(nodeArr[0][0], nodeArr[0][1], nodeArr[0][2]);
-        for(int i = 1; i < nodeArr.length; i++) {
-            insertNode(root, nodeArr[i]);
-        }
-        return root;
+    public void postfix(Node root, ArrayList<Integer> resultList) {        
+        if(root.leftNode != null) postfix(root.leftNode, resultList);
+        if(root.rightNode != null) postfix(root.rightNode, resultList);
+        resultList.add(root.nodeNumber);
     }
     
-    public void insertNode(Node parent, int[] newNode) {
-        if(newNode[0] < parent.x) {
-            if(parent.leftNode == null) parent.leftNode = new Node(newNode[0], newNode[1], newNode[2]);
-            else insertNode(parent.leftNode, newNode);
-        } else {
-            if(parent.rightNode == null) parent.rightNode = new Node(newNode[0], newNode[1], newNode[2]);
-            else insertNode(parent.rightNode, newNode);
+    public void insertNode(Node node, int x, int y, int nodeNumber){
+        if(node.x > x){ // 모든 노드는 서로 다른 x값을 가진다
+            if(node.leftNode == null) {
+                node.leftNode = new Node(x, y, nodeNumber);
+            } else {
+                insertNode(node.leftNode, x, y, nodeNumber);
+            }
+        }  else {
+            if(node.rightNode == null) {
+                node.rightNode = new Node(x, y, nodeNumber);
+            } else {
+                insertNode(node.rightNode, x, y, nodeNumber);
+            }
         }
     }
     
-    class Node {
-        int nodeNumber; 
+    class Node{
         int x;
         int y;
+        int nodeNumber;
+        
         Node leftNode;
         Node rightNode;
         
         public Node(int x, int y, int nodeNumber) {
-            this.nodeNumber = nodeNumber;
             this.x = x;
             this.y = y;
+            this.nodeNumber = nodeNumber;
         }
-    }
-    
-    class Tree {
-        Node root;
     }
 }
